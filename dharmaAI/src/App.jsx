bcff7af6678d0aea00e6e49f6c579f124a269886
Sumit Debroy <sroydeb2015@gmail.com>
2025-07-11 20:54:35 +0530

    moved to mainjsx

--- dharmaAI/src/App.jsx
+++ dharmaAI/src/App.jsx
@@ -1,3 +1,20 @@
-import App from "./components/App";
+import { Text } from "./components/Text";
+import "./App.css";
+import { Suspense } from "react";
+import React from "react";
+
+function Loading() {
+  return <div>Loading...</div>;
+}
+
+function App() {
+  return (
+    <Suspense fallback={<Loading />}>
+      <div>
+        <Text />
+      </div>
+    </Suspense>
+  );
+}
 
 export default App;
