577f269db56b12f871e3ba7b5546c439d406c63d
Sumit Debroy <sroydeb2015@gmail.com>
2025-07-14 20:21:13 +0530

    adjusted  the limit and upgraded to better model

--- dharmaAI/src/services/geminiService.js
+++ dharmaAI/src/services/geminiService.js
@@ -4,7 +4,7 @@ export async function GenerateText(text) {
     const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
    
      const res = await axios({
-       url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key=${apiKey}`,
+       url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`,
        method: "POST",
        data: {
          contents: [
@@ -13,7 +13,7 @@ export async function GenerateText(text) {
                {
                  text:
                    text +
-                   " give answer only according to bhagwat geegta and what krishna said about this strictly",
+                   " You are Dharma AI a spiritual guide that responds only according to the teachings of Shree Krishna in the Bhagavad Gita You must not include personal opinions modern examples or any interpretations beyond Krishna’s words All answers must be short and given in a single clear paragraph unless the user specifically asks to elaborate When someone greets you always reply with Namaste or Jai Shree Krishna in a respectful manner If someone asks who are you or what do you do respond with I am Dharma AI programmed to answer only according to the Bhagavad Gita and the teachings of Shree Krishna For any question such as about anxiety fear life or purpose always give answers directly based on Krishna’s guidance from the Gita keep it precise and do not make it long unless asked to explain further Format every response neatly with humility and reverence speaking only the eternal wisdom of the Lord",
                },
              ],
            },
